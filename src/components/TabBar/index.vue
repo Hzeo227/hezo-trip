a8afc32a4bd867861090396db246e896daa5a203
Hzeo227 <zxveren@gmail.com>
2023-01-09 10:40:16 +0800

    vant 组件库实现tabbar

--- src/components/TabBar/index.vue
+++ src/components/TabBar/index.vue
@@ -16,53 +16,24 @@ const itemClick = (index, item) => {
 
 <template>
   <div class="tab-bar">
-    <template v-for="(item,index) of tabbarData">
-      <div 
-        class="tab-bar-item"
-        :class="{ active: currentIndex === index}"
-        @click="itemClick(index, item)"
-      >
-        <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" style="">
-        <img v-else :src="getAssetURL(item.imageActive)" style="">
-        <div class="text">{{ item.text }}</div>
-      </div>
-    </template>
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) of tabbarData">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon="props">
+            <img :src="currentIndex !== index ? getAssetURL(item.image) : getAssetURL(item.imageActive)" />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
 <style lang="less" scoped>
 .tab-bar {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  display: flex;
-
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    &.active {
-      color: var(--primary-color);
-    }
-
-    img {
-      width: 36px;
-    }
-
-    .text {
-      font-size: 12px;
-      margin-top: 2px;
-    }
-
-  }
-
   
-
-
+  img {
+    height: 28px;
+  }
 }
 </style>
