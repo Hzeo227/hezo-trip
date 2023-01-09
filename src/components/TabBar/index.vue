5e92d733d41073799289f6ba922450302491813b
Hzeo227 <zxveren@gmail.com>
2023-01-09 10:04:39 +0800

    tabbar 功能实现

--- src/components/TabBar/index.vue
+++ src/components/TabBar/index.vue
@@ -1,16 +1,29 @@
 <script setup>
 import tabbarItems from '@/assets/data/tabbar'
 import { getAssetURL } from '@/utils/load_assets'
+import { useRouter } from 'vue-router';
+import { ref } from 'vue'
 
 const tabbarData = tabbarItems
+const currentIndex = ref(0)
+const router = useRouter()
+const itemClick = (index, item) => {
+  currentIndex.value = index
+  router.push(item.path)
+}
 
 </script>
 
 <template>
   <div class="tab-bar">
     <template v-for="(item,index) of tabbarData">
-      <div class="tab-bar-item">
-        <img :src="getAssetURL(item.image)" style="">
+      <div 
+        class="tab-bar-item"
+        :class="{ active: currentIndex === index}"
+        @click="itemClick(index, item)"
+      >
+        <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" style="">
+        <img v-else :src="getAssetURL(item.imageActive)" style="">
         <div class="text">{{ item.text }}</div>
       </div>
     </template>
@@ -32,15 +45,24 @@ const tabbarData = tabbarItems
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    &.active {
+      color: var(--primary-color);
+    }
+
+    img {
+      width: 36px;
+    }
+
+    .text {
+      font-size: 12px;
+      margin-top: 2px;
+    }
+
   }
 
-  img {
-    width: 36px;
-  }
+  
 
-  .text {
-    font-size: 12px;
-    margin-top: 2px;
-  }
+
 }
 </style>
